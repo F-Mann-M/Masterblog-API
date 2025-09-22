2d1dcf18baf40f78899826b6991fba49e64014d3
Friedemann Muecket <friedemann@muecket.de>
2025-09-22 10:23:53 +0200

    added delete feature

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,18 +1,49 @@
-from flask import Flask, jsonify
+import json
+import os
+from flask import Flask, jsonify, request, url_for
 from flask_cors import CORS
+import post_storage
+from backend.post_storage import fetch_post_by_id
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
-
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    return jsonify(POSTS)
+    posts = post_storage.get_posts()
+    if not posts:
+        return jsonify({ "error" : "database not found."})
+    return jsonify(posts)
+
+
+@app.route("/api/posts", methods=["POST"])
+def add_post():
+    # Get new post data
+    new_post = request.get_json()
+
+    # return error if new_post invalid
+    if not new_post:
+        return jsonify({"error": "Invalid or missing JSON data"}), 400
+
+    # add new data to database (json file)
+    title = new_post.get("title")
+    content = new_post.get("content")
+    post_storage.add_post(title, content)
+    posts = post_storage.get_posts()
+    return jsonify(posts)
+
+
+@app.route("/api/posts/<int:post_id>", methods=["DELETE"])
+def delete_post(post_id):
+    post = post_storage.fetch_post_by_id(post_id)
+
+    if post_storage.delete_post(post_id):
+        return jsonify({"message": f"Post with id {post_id} has been deleted successfully."})
+    else:
+        return jsonify({"error": f"Post with id {post_id} not found."})
+
+
 
 
 if __name__ == '__main__':
---
+++ backend/post_storage.py
@@ -0,0 +1,77 @@
+import json
+import os
+
+
+def load_file(file_path):
+    """loads json and returns as list of dicts. If there is no json yet of file is empty return empty list"""
+    if not os.path.exists(file_path):
+        print(f"{file_path} does not exist")
+        return []
+
+    with open(file_path, "r") as handel:
+        try:
+            posts = json.load(handel)
+            print("file loaded: ")
+            return posts
+        except Exception as e:
+            print(f"Error: {e}")
+            return []
+
+
+def get_posts():
+    """loads jason and returns list of dicts of the posts"""
+    posts = load_file("data/post_data.json")
+    if not posts:
+        print("Database empty.")
+        posts = []
+    return posts
+
+
+def get_new_post_id():
+    """ Goes through all dicts of a list and looks for the highest id. Returns new id."""
+    posts = get_posts()
+    if posts:
+        return max(post["id"] for post in posts) + 1
+    else:
+        return 1  # if no posts yet return id 1
+
+
+def add_post(title, content):
+    """load json, add new post along with author, title, content"""
+    post_id = get_new_post_id()
+    posts = get_posts()
+    new_post_dict = {"id": post_id, "title": title, "content": content}
+    posts.append(new_post_dict)
+    write_file(posts)
+
+
+def delete_post(post_id):
+    """ takes in post id and deletes dictionary with this id"""
+    posts = get_posts()
+    for post in posts:
+        if post["id"] == post_id:
+            posts.remove(post)
+    write_file(posts)
+
+
+def fetch_post_by_id(post_id):
+    """ takes in an id and returns associated dictionary (post)."""
+    posts = get_posts()
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+
+
+def update_post(title, content, post_id):
+    """ takes in author, title, content and id and updates post with id"""
+    posts = get_posts()
+    for post in posts:
+        if post["id"] == post_id:
+            post["title"] = title
+            post["content"] = content
+    write_file(posts)
+
+
+def write_file(file):
+    with open("data/post_data.json", "w", encoding='utf-8') as handel:
+        json.dump(file, handel, ensure_ascii=False, indent=4)
