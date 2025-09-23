83cde8acb657c944b4da6efd69cd99d9ef3b3b4c
Friedemann Muecket <friedemann@muecket.de>
2025-09-23 04:48:50 +0200

    added search feature to backend.py

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -13,6 +13,7 @@ CORS(app)  # This will enable CORS for all routes
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    """ Gets all posts form json and returns as json object"""
     posts = post_storage.get_posts()
     if not posts:
         return jsonify({ "error" : "database not found."})
@@ -21,6 +22,11 @@ def get_posts():
 
 @app.route("/api/posts", methods=["POST"])
 def add_post():
+    """
+    Takes in a POST request with title and content
+    adds new post to database
+    """
+
     # Get new post data
     new_post = request.get_json()
 
@@ -38,8 +44,7 @@ def add_post():
 
 @app.route("/api/posts/<int:post_id>", methods=["DELETE"])
 def delete_post(post_id):
-    post = post_storage.fetch_post_by_id(post_id)
-
+    """ Takes in post id and deletes post form json"""
     if post_storage.delete_post(post_id):
         return jsonify({"message": f"Post with id {post_id} has been deleted successfully."})
     else:
@@ -48,14 +53,22 @@ def delete_post(post_id):
 
 @app.route("/api/posts/<int:post_id>", methods=["PUT"])
 def update_post(post_id):
+    """
+    Takes in post ID and gets json object (title, content).
+    Updates post with post_id in database
+    and returns updated database as json object
+    """
+    # Get Data
     updated_post = request.get_json()
 
+    # return error if json object invalid
     if not updated_post:
         return jsonify({"error": "Invalid or missing JSON data"}), 400
 
     title = updated_post.get("title")
     content = updated_post.get("content")
 
+    # Update database
     post_storage.update_post(title, content, post_id)
     if not post_storage.fetch_post_by_id(post_id):
         return jsonify( {"error": f"Post with ID {post_id} not found"})
@@ -63,19 +76,26 @@ def update_post(post_id):
     posts = post_storage.get_posts()
     return jsonify(posts)
 
+@app.route("/api/posts/search", methods=["GET"])
+def search_post():
+    """Takes in search query (title, content). Goes through post title and content and returns search results"""
+    title = request.args.get("title")
+    content = request.args.get("content")
 
-# Error Handling
-@app.errorhandler(400)
-def error_400():
-    pass
+    posts = post_storage.get_posts()
 
-@app.errorhandler(404)
-def error_404():
-    pass
+    search_results = []
 
-@app.errorhandler(500)
-def error_400():
-    pass
+    for post in posts:
+        if title and title.lower() in post["title"].lower():
+            search_results.append(post)
+        if content and content.lower() in post["content"].lower():
+            search_results.append(post)
+    if search_results:
+        return jsonify(search_results)
+    else:
+        return jsonify({"message" : "nothing found :("})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
